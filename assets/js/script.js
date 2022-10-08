7daa8e3dbb480609f2f9e7a35397c618d39b33be
Jose Plasencia <josejpd3@gmail.com>
2022-10-08 03:03:51 -0400

    added saving and retrieving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,22 @@ setInterval (function() {
         $("textarea").addClass("future")
     }
 }, 1000)
+let saveBtn = $(".saveBtn");
+// Saves inputed text when clicked on save 
+saveBtn.on("click", save)
+function save(event) {
+    event.preventDefault();
+    let buttonNum = $(this).attr('name');
+    let textInput = $(this).parent().find('textarea').val();
+    localStorage.setItem(buttonNum, textInput);
+}; 
+// retrives values from all inputs
+$('#9').val(localStorage.getItem("9", "textInput"));
+$('#10').val(localStorage.getItem("10", "textInput"));
+$('#11').val(localStorage.getItem("11", "textInput"));
+$('#12').val(localStorage.getItem("12", "textInput"));
+$('#1').val(localStorage.getItem("1", "textInput"));
+$('#2').val(localStorage.getItem("2", "textInput"));
+$('#3').val(localStorage.getItem("3", "textInput"));
+$('#4').val(localStorage.getItem("4", "textInput"));
+$('#5').val(localStorage.getItem("5", "textInput"));
